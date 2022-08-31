b7ff6719b5ff15a1c7ac52fd670e21566290d779
tonykk23 <gabrielsandeski2006@gmail.com>
2022-08-31 20:33:11 -0300

    repositorio criado

--- exercicio17.cs
+++ exercicio17.cs
@@ -1,5 +1,21 @@
 using System;
 
 class programa{
-    
+    public static void Main(string[] args){
+        string nome;
+        int idade, AnosNaEmpresa;
+        double salario;
+
+        Console.WriteLine("digite seu nome: ");
+
+        Console.WriteLine("digite a sua idade: ");
+        idade = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("a quantos você trabalha na empressa?");
+        AnosNaEmpresa = double.Parse(Console.ReadLine());
+
+        Console.WritwLine("qual o seu salario?");
+        salario = double.Parse(Console.ReadLine());
+    }
+   
 }
